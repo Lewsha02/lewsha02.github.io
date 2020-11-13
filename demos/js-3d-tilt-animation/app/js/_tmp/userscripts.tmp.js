ff3b38d5eccf46e7c75fa2f59499f6a2fdb9694c
Egor Ney <egorney@MacBook-Pro-Egor.local>
2020-11-13 22:47:36 +0300

    add 3d-tilt-animation

---
+++ demos/js-3d-tilt-animation/app/js/_tmp/userscripts.tmp.js
@@ -0,0 +1,70 @@
+"use strict";
+
+document.addEventListener('DOMContentLoaded', function () {
+  // Custom JS
+  var body = document.querySelector('body');
+  cx = window.innerWidth / 2, cy = window.innerHeight / 2;
+  body.addEventListener('mousemove', function (e) {
+    clientX = e.pageX, clientY = e.pageY; // console.log(e.pageX + ' / ' + e.pageY);
+
+    request = requestAnimationFrame(updateMe);
+  });
+
+  function updateMe() {
+    dx = clientX - cx;
+    dy = clientY - cy;
+    tiltx = dy / cy;
+    tilty = dx / cx;
+    radius = Math.sqrt(Math.pow(tiltx, 2) + Math.pow(tilty, 2));
+    degree = radius * 12;
+    gsap.to('.content', 1, {
+      transform: "rotate3d( ".concat(tiltx, ", ").concat(tilty, ", 0, ").concat(degree, "deg )")
+    });
+  }
+
+  gsap.to('.card', {
+    zoom: .98
+  });
+  gsap.to('.l_main', {
+    opacity: 1,
+    duration: .1
+  });
+  gsap.to('.l2_main', {
+    opacity: 1,
+    left: -10,
+    top: 10,
+    duration: .25,
+    delay: .25
+  });
+  gsap.to('.l3_main', {
+    opacity: 1,
+    left: -20,
+    top: 20,
+    duration: .25,
+    delay: .25
+  });
+  gsap.to('.card-russia', {
+    opacity: .07,
+    duration: .1
+  });
+  gsap.to('.card-logo_w', {
+    opacity: 1,
+    duration: .25
+  });
+  gsap.to('.card-chip', {
+    opacity: 1,
+    duration: .25
+  });
+  gsap.to('.card-valid', {
+    opacity: 1,
+    zoom: 1,
+    duration: .1,
+    delay: .25
+  });
+  gsap.to('.card-number-holder', {
+    opacity: 1,
+    zoom: 1,
+    duration: .1,
+    delay: .25
+  });
+});
